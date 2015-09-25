e55d466790a41450478f425bec306e241ddc0c5d
caseyconley <plazma4545@gmail.com>
2015-09-24 22:12:20 -0700

    Draw a circle!

--- src/caseyface.c
+++ src/caseyface.c
@@ -2,19 +2,59 @@
 
 typedef struct {
   Window *mywindow;
+  Layer *container_layer;
 
 } CaseyFaceData;
 
+static void prv_print_grect(const char *rect_name, GRect rect_to_print) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "%s\t\t - x: %d\t\ty: %d\t\tw: %d\t\th: %d", rect_name,
+          rect_to_print.origin.x, rect_to_print.origin.y, rect_to_print.size.w, rect_to_print.size.h);
+}
+
+static GRect prv_shim_grect_centered_from_polar(GRect rect, GOvalScaleMode scale_mode, int32_t angle, GSize size) {
+  const GPoint point_centered_from_polar = gpoint_from_polar(rect, scale_mode, angle);
+  return GRect(point_centered_from_polar.x - (size.w / 2),
+               point_centered_from_polar.y - (size.h / 2), size.w, size.h);
+}
+
+static void container_layer_update_proc(Layer *layer, GContext *ctx)
+{
+  GRect container_rect = layer_get_bounds(layer);
+  container_rect = grect_inset(container_rect, GEdgeInsets(-10));
+
+  //could be min(w/4) for rectangle watch
+  int minute_circle_diameter = container_rect.size.w/4;
+  GSize minute_circle_rect_size = GSize(minute_circle_diameter, minute_circle_diameter);
+
+  GRect minute_circle_container_rect = grect_inset(container_rect, GEdgeInsets(container_rect.size.w / 3));
+  GRect minute_circle_rect = prv_shim_grect_centered_from_polar(minute_circle_container_rect, GOvalScaleModeFitCircle,
+                                                                DEG_TO_TRIGANGLE(30), minute_circle_rect_size);
+
+  graphics_context_set_fill_color(ctx, GColorSpringBud);
+  graphics_fill_radial(ctx, minute_circle_rect, GOvalScaleModeFitCircle, minute_circle_diameter/2, 0, TRIG_MAX_ANGLE);
+}
+
 static void main_window_load(Window *window)
 {
+  CaseyFaceData *data = window_get_user_data(window);
 
+  Layer *root_layer = window_get_root_layer(window);
+  data->container_layer = layer_create(layer_get_bounds(root_layer));
+
+  layer_set_update_proc(data->container_layer, container_layer_update_proc);
+
+  layer_add_child(root_layer, data->container_layer);
 }
 
 static void main_window_unload(Window *window)
 {
+  CaseyFaceData *data = window_get_user_data(window);
+
+  layer_destroy(data->container_layer);
+
   window_destroy(window);
 
-  CaseyFaceData *data = window_get_user_data(window);
+
   free(data);
 }
 
